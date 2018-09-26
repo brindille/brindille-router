acf96e142716a61bb9e82d3640ebc0ca908ab28c
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2018-09-26 11:27:41 +0200

    new build with peer deps

--- rollup.config.js
+++ rollup.config.js
@@ -17,12 +17,11 @@ export default {
       // these values can also be regular expressions
       // include: /node_modules/
 
-      // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: false,  // Default: true
-
+      
       // explicitly specify unresolvable named exports
       // (see below for more details)
       // namedExports: { './module.js': ['foo', 'bar' ] },  // Default: undefined
     })
-  ]
+  ],
+  external: ['brindille-component', 'deep-equal', 'dush']
 };
